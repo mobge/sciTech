bd956bac749c4a80b0fcf251cc9e41cb099cd7e0
unknown <ozlem.kacar90@hotmail.com>
2019-04-30 19:02:09 +0300

    guncelleme

---
+++ src/app/firebase.ts
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyA3x65haGj9_a0LN-ujthRuxTSYxRtYnzE",
+    authDomain: "scitech-a8f1c.firebaseapp.com",
+    databaseURL: "https://scitech-a8f1c.firebaseio.com",
+    projectId: "scitech-a8f1c",
+    storageBucket: "scitech-a8f1c.appspot.com",
+    messagingSenderId: "1088829867500"
+}
+export default config //yeni düzenleme
---
+++ src/app/uploader/uploader.page.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Http } from '@angular/http';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { firestore } from 'firebase/app';
+import { UserService } from '../user.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-uploader',
+  templateUrl: './uploader.page.html',
+  styleUrls: ['./uploader.page.scss'],
+})
+export class UploaderPage implements OnInit {
+
+  imageURL: string;
+  desc: string
+
+  busy: boolean = false;
+
+  @ViewChild('fileButton') fileButton
+
+  constructor(
+    public http: Http, 
+    public afstore: AngularFirestore, 
+    public user: UserService, 
+    private alertController: AlertController,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+  
+  async createPost()
+  {
+    this.busy=true
+    const image=this.imageURL
+    const desc=this.desc
+
+    this.afstore.doc(`users/${this.user.getUID()}`).update({
+      posts: firestore.FieldValue.arrayUnion(image) 
+    })
+
+    this.afstore.doc(`posts/${image}`).set({
+      desc,
+      author: this.user.getUsername(),
+      likes: []
+    })
+
+    this.busy=false
+    this.imageURL=""
+    this.desc=""
+
+
+
+    const alert= await this.alertController.create({
+      header: 'Bitti',
+      message: 'Gönderiniz başarıyla oluşturuldu!',
+      buttons: ['OK!']
+    })
+
+    await alert.present()
+
+    this.router.navigate(['tabs/feed'])
+
+  }
+
+  uploadFile() {
+    this.fileButton.nativeElement.click()
+  }
+
+  fileChanged(event) {
+    this.busy=true;
+
+    const files=event.target.files;
+  
+    const data=new FormData()
+    data.append('file',files[0])
+    data.append('UPLOADCARE_STORE', '1')
+    data.append('UPLOADCARE_PUB_KEY','33203dfc97a0f5b0a657')
+
+    this.http.post('https://upload.uploadcare.com/base/', data)
+    .subscribe(event => {
+      console.log(event)
+        this.imageURL=event.json().file
+        this.busy=false;
+    })
+  }
+}
